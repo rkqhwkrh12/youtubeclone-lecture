f11aebba15ee11abd9c6657b36da1c6472048a1d
rkqhwkrh12 <140291672+rkqhwkrh12@users.noreply.github.com>
2023-08-26 23:12:00 +0900

    is no

---
+++ src/routers/videoRouter.js
@@ -0,0 +1,10 @@
+import express from "express";
+//video Router도 만들어 주구.
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+export default videoRouter;
+dfsd
