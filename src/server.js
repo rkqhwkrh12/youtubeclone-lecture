1d391f833406391520b8d3ca187a4d2be5bf42ab
rkqhwkrh12 <140291672+rkqhwkrh12@users.noreply.github.com>
2023-08-26 09:59:50 +0900

    hi-this-is-test

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const login = (req, res) => {
 app.use(logger);
 app.get("/", home);
 app.get("/login", login);
-
+console.log("Hello");
 
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
