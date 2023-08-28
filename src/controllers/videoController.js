9224a29e89a6978fd072136e19a4377eb204a229
rkqhwkrh12 <140291672+rkqhwkrh12@users.noreply.github.com>
2023-08-28 12:39:16 +0900

    5lecture-pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -5,12 +5,42 @@ const watch = (req, res) => res.send("Watch");
 const edit = (req, res) => res.send("Edit");
 //export를 붙임으로써 한 파일이 여러개를 익스포트 할 수 있음.
 */
-export const trending = (req, res) => res.send("Home Page Videos");
+
+export const trending = (req, res) => {
+    const videos = [
+        {
+            title: "First Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Second Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        {
+            title: "Third Video",
+            rating: 5,
+            comments: 2,
+            createdAt: "2 minutes ago",
+            views: 59,
+            id: 1,
+        },
+        
+    ];
+    res.render("home", {pageTitle: "Home", potato: "potato",videos}) //render()안에 pug의 파일명을 넣어준다.
+};
 export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
+    return res.render("watch");
 };
 export const edit = (req, res) => {
-    return res.send("Edit");
+    return res.render("edit");
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
