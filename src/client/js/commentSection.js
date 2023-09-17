8a2736ccb9502f89d6f431a85b1fe90b036f4165
rkqhwkrh12 <140291672+rkqhwkrh12@users.noreply.github.com>
2023-09-17 11:16:10 +0900

    UseJSON

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,15 +1,31 @@
 //프론트엔드에서 작업하기 위해서 client/js에 추가함
 const videoContainer = document.getElementById("videoContainer"); // 여기서 받아온 video 아이디를 이용해서 어떤 비디오에 댓글이 달렸는지 판단.
 const form = document.getElementById("commentForm"); //watch.pug에 있는 form을 이용하기 위해서 id를 가지고 옴.
-const textarea = form.querySelector("textarea"); //달린 댓글을 컨트롤하기 위해서
-const btn = form.querySelector("button");
+
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value; //달린 댓글 컨트롤
-  const video = videoContainer.dataset.id; //어떤 비디오에 추가 되었는지
+  const videoId = videoContainer.dataset.id; //어떤 비디오에 추가 되었는지
+
+  if (text === "" || text.trim() === "") {
+        return;
+    }
+  //작성한 댓글을 backend로 보내야 함.
+  //그러기 위해서 router에 post를 추가해 주어야 한다.
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+    },
+    body: JSON.stringify({text}), //middleware에서 json을 설정해 주어야 사용가능.
+  });
 };
 
 //click으로 하면 button이 자동으로 제출해버려서 
 //submit속성으로 지정함.
-form.addEventListener("submit", handleSubmit);
+//login 되어 있을때만 댓글을 달 수 있는 창이 나와야 하므로 다음과 같이 코드작성.
+if(form) {
+    form.addEventListener("submit", handleSubmit);
+}
--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -1,11 +1,11 @@
 //조회수를 조회를 위한 라우터
 import express from "express";
-import { registerView } from "../controllers/videoController";
+import { registerView, createComment } from "../controllers/videoController";
 
 const apiRouter = express.Router();
 
 //요청을 보내더라도 url을 바꾸지 않고 템플릿을 렌더링하지 않기 위해서
 //POST만 작성. >> 백엔드에서만 조회수를 업데이트해줌
 apiRouter.post("/videos/:id([0-9a-f]{24})/view", registerView);
-
+apiRouter.post("/videos/:id([0-9a-f]{24})/comment", createComment);
 export default apiRouter;
